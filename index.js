af16f85556537d1682dfddffa8fd75981d69120d
Markus Lundin <m.lundin.1986@gmail.com>
2017-11-20 15:49:45 +0100

    Lägg till protractor helpers

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     helpers: require('./lib/helpers.js'),
     HSAusers: require('./lib/hsa_users.js'),
     testdataHelper: require('./lib/testdataHelper.js'),
-	uiHelpers: require('./lib/uiHelpers.js')
+	uiHelpers: require('./lib/uiHelpers.js'),
+	protractorHelpers: require('./lib/protractorHelpers.js')
 };
