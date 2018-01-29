d9982049004d49ca9b942aeb9f49bcc0989e1714
Markus Lundin <m.lundin.1986@gmail.com>
2018-01-29 11:32:20 +0100

    Ta bort uihelpers och merga in fortfarande användbara funktioner i protractorHelpers

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ module.exports = {
     helpers: require('./lib/helpers.js'),
     HSAusers: require('./lib/hsa_users.js'),
     testdataHelper: require('./lib/testdataHelper.js'),
-	uiHelpers: require('./lib/uiHelpers.js'),
 	protractorHelpers: require('./lib/protractorHelpers.js')
 };
